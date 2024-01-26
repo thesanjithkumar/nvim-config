43986385c4811a677f8708fe7b8b1b897bb1e294
Sanjth Kumar <53137451+thesanjithkumar@users.noreply.github.com>
2024-01-26 22:52:08 +0530

    added nvim dap virtual text

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -53,6 +53,14 @@ local plugins = {
     end
   },
 
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+
    {
     "hrsh7th/nvim-cmp",
     opts = function()
